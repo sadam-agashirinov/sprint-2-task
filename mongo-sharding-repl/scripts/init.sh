ebdaae950679b9442c3760c91a7cc4adeef880fd
sadam-agashirinov <sadam.agashirinov@gmail.com>
2025-02-23 13:24:49 +0300

    feat: Задача 3. Репликация.

--- mongo-sharding-repl/scripts/init.sh
+++ mongo-sharding-repl/scripts/init.sh
@@ -27,6 +27,8 @@ rs.initiate(
       _id : "shard1",
       members: [
         { _id : 0, host : "shard1:27018" },
+        { _id : 1, host : "shard1-repl1:27021" },
+        { _id : 2, host : "shard1-repl2:27022" },
       ]
     }
 );
@@ -39,6 +41,8 @@ rs.initiate(
       _id : "shard2",
       members: [
         { _id : 0, host : "shard2:27019" },
+        { _id : 1, host : "shard2-repl1:27023" },
+        { _id : 2, host : "shard2-repl2:27024" },
       ]
     }
 );
