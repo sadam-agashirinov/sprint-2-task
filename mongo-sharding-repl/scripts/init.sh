61358d27246f59ccd410687c2a694dc7094de842
sadam-agashirinov <sadam.agashirinov@gmail.com>
2025-02-23 11:19:02 +0300

    feat: Задание 3. Репликация

---
+++ mongo-sharding-repl/scripts/init.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+###
+# Инициализируем сервер конфигурации
+###
+
+docker compose exec -T configSrv mongosh --port 27017 <<EOF
+rs.initiate(
+  {
+    _id : "config_server",
+       configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);
+exit();
+EOF
+
+###
+# Инициализируем шарды
+###
+
+docker compose exec -T shard1 mongosh --port 27018 <<EOF
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" },
+      ]
+    }
+);
+exit();
+EOF
+
+docker compose exec -T shard2 mongosh --port 27019 <<EOF
+rs.initiate(
+    {
+      _id : "shard2",
+      members: [
+        { _id : 0, host : "shard2:27019" },
+      ]
+    }
+);
+exit();
+EOF
+
+###
+# Ждем пока роутер запустится и станет доступен.
+# Есть проблема долгого старта.
+###
+
+until docker compose exec -T mongos_router mongosh --port 27020 --eval 'db.runCommand({ping: 1})'; do
+  echo "Waiting for mongos_router..."
+  sleep 1
+done
+
+###
+# Инцициализируйте роутер и наполните его тестовыми данными
+###
+
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+sh.addShard("shard1/shard1:27018");
+sh.addShard("shard2/shard2:27019");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+exit();
+EOF
